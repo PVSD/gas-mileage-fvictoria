1878ea8774155c352d20e6aa816b531426658d66
fvictoria <felipevictoria630@yahoo.com>
2018-10-11 22:41:52 -0400

    the need for sleep

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,29 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+
+    public int miles = 0;
+    public int gallons = 0;
+    public int mpg = 0;
+
+    public Automobile (int milesPerGallon)
+    {
+        mpg = milesPerGallon;
+    }
+
+    public void takeTrip (int mileage)
+    {
+        this.miles = miles;
+        gallons = mileage - mpg;
+    }
+
+    public void fillUp (int gas)
+    {
+        gas = gas + gallons;
+    }
+
+    public double reportFuel ()
+    {
+        return gallons;
+    }
 }
